cb8ae33acdf4826fc37ccc4c312ebe8b82732f87
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-01-20 21:41:15 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript form action</h2>
+
+<form action="/action_page.php" method="post">
+  <input type="text" name="fname" required>
+  <input type="submit" value="Submit">
+</form>
+
+<p>If you click the submit button, without filling out the text field,
+your browser will display an error message.</p>
+
+</body>
+</html>
